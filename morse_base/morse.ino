7328d52e0f9d609d47b42e9c8900fd33e07607ef
Paulo Santos <pauloxrms@gmail.com>
2022-11-23 12:36:15 -0300

    base para construção dos códigos

--- morse_base/morse.ino
+++ morse_base/morse.ino
@@ -1,7 +1,7 @@
 /**
  * @file morse-tx.ino
  * @author Paulo Roberto (pauloxrms@gmail.com/pauloroberto.santos@edge.ufal.br)
- * @brief Sketch de exemplo em como implementar um codificador morse em Arduino.
+ * @brief Sketch de base para as definições do morse em Arduino.
  * @version 0.1
  * @date 2022-11-22
  *
@@ -112,70 +112,10 @@ char letters[_LETTERS_AMOUNT][7] = {
     ".--.-."  /*< @ */
 };
 
-/**
- * @brief Função que dada uma string em morse, repete o padrão no led.
- *
- * @param letter string com pontos e traços que representa a letra a ser
- * transmitida.
- */
-void transmit_from_str(char *letter) {
-  int i = 0;
-
-  while (letter[i] != '\0') { /** Enquanto a string não acabou... */
-
-    digitalWrite(LED_PIN, HIGH); /** Liga o LED */
-
-    if (letter[i] == '.') { /** Escolhe o tempo para o ponto ou o traço. */
-      delay(DOT_DELAY);
-    } else if (letter[i] == '-') {
-      delay(DASH_DELAY);
-    }
-
-    digitalWrite(LED_PIN, LOW);   /** Desliga o LED */
-    delay(IN_LETTER_SPACE_DELAY); /** Dá o tempo necessário entre pontos e
-                                     traços de uma letra. */
-
-    i++;
-  }
+void setup() {
+  pinMode(LED_PIN, OUTPUT); 
 }
-
-/**
- * @brief Camada de abstração da função de transmição.
- *
- * @param letter Letra enumerada a ser transmitida.
- * @param is_last Indica se esta letra é o fim de uma palavra.
- */
-void transmit_letter(letter_t letter, bool is_last) {
-  transmit_from_str(letters[letter]);
-  if (is_last) {
-    delay(WORD_SPACE_DELAY); /** Caso seja a última letra, dá o tempo de um fim
-                                de palavra*/
-  } else {
-    delay(OUT_LETTER_SPACE_DELAY); /** Caso seja uma letra interna da palavra*/
-  }
-}
-
-/**
- * @brief Exemplo de função que envia uma palavra.
- *
- */
-void transmit_sos() {
-  transmit_letter(S, false);
-  transmit_letter(O, false);
-  transmit_letter(S, true);
-}
-
-void setup() { pinMode(LED_PIN, OUTPUT); }
 
 void loop() {
-  transmit_from_str(letters[S]);
-  delay(OUT_LETTER_SPACE_DELAY);
-
-  transmit_from_str(letters[O]);
-  delay(OUT_LETTER_SPACE_DELAY);
-
-  transmit_from_str(letters[S]);
-  delay(WORD_SPACE_DELAY);
-
-  transmit_sos();
+  
 }
